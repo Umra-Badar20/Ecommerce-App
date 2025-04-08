e19886a239e79ee7122439ef086e2c031681f021
Umra-Badar20 <umrabadar555@gmail.com>
2025-04-08 10:39:38 +0500

    Configured for deployment

--- backend/index.mjs
+++ backend/index.mjs
@@ -6,6 +6,7 @@ import productRoutes from "./routes/productRoutes.mjs"
 import chalk from "chalk";
 import cors from "cors";
 import connectToDB from "./db/index.mjs";
+import path from 'path';
 
 //Connecting MongoDB
 connectToDB()
@@ -20,6 +21,10 @@ app.use(
 	}),
 );
 
+const __dirname = path.resolve();
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(express.json());
 const port = 5000;
@@ -27,6 +32,9 @@ app.use("/api/auth",userRoutes)
 app.use("/api/cart",cartRoutes)
 app.use('/api/products', productRoutes);
 
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 app.use("/", (req, res, next) => {
   console.log("Request URL:", req.url, "method: ", req.method);
   next();
